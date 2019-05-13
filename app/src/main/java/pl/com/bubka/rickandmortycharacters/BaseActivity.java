c71f3cdaf844cc02d8fe4ee97113a11549d655eb
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-13 19:38:57 +0200

    Added base activity

---
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/BaseActivity.java
@@ -0,0 +1,33 @@
+package pl.com.bubka.rickandmortycharacters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+public class BaseActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        progressBar = constraintLayout.findViewById(R.id.progressBar);
+
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(layoutResID);
+    }
+
+    public void showProgressBar(){
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void hideProgressBar(){
+        progressBar.setVisibility(View.GONE);
+    }
+
+}
---
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/RickAndMortyApplciation.java
@@ -0,0 +1,11 @@
+package pl.com.bubka.rickandmortycharacters;
+
+import android.app.Application;
+
+public class RickAndMortyApplciation extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
