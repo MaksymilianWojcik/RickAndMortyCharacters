58065725d37eb33b407e311a5056acd5fe0179db
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-13 19:46:21 +0200

    Prepared executors

---
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/AppExecutors.java
@@ -0,0 +1,40 @@
+package pl.com.bubka.rickandmortycharacters;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+
+    private static AppExecutors instance;
+
+    public static AppExecutors getInstance(){
+        if (instance == null){
+            instance = new AppExecutors();
+        }
+        return instance;
+    }
+
+    private final Executor mainThreadExecutor = new MainThreadExecutor();
+    private final Executor diskIO = Executors.newSingleThreadExecutor();
+
+    public Executor diskIO(){
+        return diskIO;
+    }
+
+    public Executor mainThread(){
+        return mainThreadExecutor;
+    }
+
+    private static class MainThreadExecutor implements Executor{
+
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+}
