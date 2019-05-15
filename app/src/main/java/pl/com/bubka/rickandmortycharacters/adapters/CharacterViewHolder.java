43ed52b40aaa13017f9afbdce379eae41d598042
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-15 20:41:42 +0200

    Added recycler view adapters

---
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/adapters/CharacterViewHolder.java
@@ -0,0 +1,40 @@
+package pl.com.bubka.rickandmortycharacters.adapters;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.RequestManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
+import pl.com.bubka.rickandmortycharacters.R;
+import pl.com.bubka.rickandmortycharacters.models.Character;
+
+public class CharacterViewHolder extends RecyclerView.ViewHolder {
+
+    RequestManager requestManager;
+    TextView name, status, species;
+    AppCompatImageView imageView;
+
+    //TODO: Caching glide - preloaders
+
+    public CharacterViewHolder(@NonNull View itemView, RequestManager requestManager) {
+        super(itemView);
+        this.requestManager = requestManager;
+
+        name = itemView.findViewById(R.id.character_name);
+        status = itemView.findViewById(R.id.character_status);
+        species = itemView.findViewById(R.id.character_species);
+        imageView = itemView.findViewById(R.id.character_image);
+    }
+
+    public void onBind(Character character){
+        requestManager.load(character.getImageUrl())
+                .into(imageView);
+        name.setText(character.getName());
+        status.setText(character.getStatus());
+        species.setText(character.getSpecies());
+    }
+
+}
---
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/adapters/LoadingViewHolder.java
@@ -0,0 +1,13 @@
+package pl.com.bubka.rickandmortycharacters.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LoadingViewHolder extends RecyclerView.ViewHolder {
+
+    public LoadingViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
