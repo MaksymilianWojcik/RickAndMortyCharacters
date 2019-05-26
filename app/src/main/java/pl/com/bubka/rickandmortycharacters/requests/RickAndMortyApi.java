33407093263f88f21e8d0db92927fb34e286b5df
Maksymilian Wojcik <maksymilian.wojcik@acasus.com>
2019-05-26 19:04:58 +0200

    Added new retrofit requests,

--- app/src/main/java/pl/com/bubka/rickandmortycharacters/requests/RickAndMortyApi.java
+++ app/src/main/java/pl/com/bubka/rickandmortycharacters/requests/RickAndMortyApi.java
@@ -1,9 +1,13 @@
 package pl.com.bubka.rickandmortycharacters.requests;
 
 import androidx.lifecycle.LiveData;
+import pl.com.bubka.rickandmortycharacters.models.Character;
+import pl.com.bubka.rickandmortycharacters.models.Episode;
+import pl.com.bubka.rickandmortycharacters.models.Location;
 import pl.com.bubka.rickandmortycharacters.requests.responses.ApiResponse;
-import pl.com.bubka.rickandmortycharacters.requests.responses.CharacterDetailsResponse;
 import pl.com.bubka.rickandmortycharacters.requests.responses.CharacterSearchResponse;
+import pl.com.bubka.rickandmortycharacters.requests.responses.EpisodeSearchResponse;
+import pl.com.bubka.rickandmortycharacters.requests.responses.LocationSearchResponse;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -17,8 +21,30 @@ public interface RickAndMortyApi {
     );
 
     @GET("api/character/{id}")
-    LiveData<ApiResponse<CharacterDetailsResponse>> getCharacterDetails(
+    LiveData<ApiResponse<Character>> searchCharacter(
             @Path("id") String id
+    );
+
+    @GET("api/location/")
+    LiveData<ApiResponse<LocationSearchResponse>> searchLocations(
+            @Query("name") String name,
+            @Query("page") String page
+    );
+
+    @GET("api/location/{id}")
+    LiveData<ApiResponse<Location>> searchLocation(
+            @Path("name") String id
+    );
+
+    @GET("api/episode/")
+    LiveData<ApiResponse<EpisodeSearchResponse>> searchEpisodes(
+            @Query("name") String name,
+            @Query("page") String page
+    );
+
+    @GET("api/episode/{id}")
+    LiveData<ApiResponse<Episode>> searchEpisode(
+            @Path("name") String id
     );
 
 }
